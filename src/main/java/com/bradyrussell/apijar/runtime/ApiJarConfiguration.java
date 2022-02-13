08575e41f67316f9ae9e49cd84c8b4c70e81466b
BradyRussell <31458670+bradyrussell@users.noreply.github.com>
2022-02-12 20:11:06 -0600

    add has(String key) to config

--- src/main/java/com/bradyrussell/apijar/runtime/ApiJarConfiguration.java
+++ src/main/java/com/bradyrussell/apijar/runtime/ApiJarConfiguration.java
@@ -3,4 +3,5 @@ package com.bradyrussell.apijar.runtime;
 public interface ApiJarConfiguration {
     String get(String key);
     void set(String key, String value);
+    boolean has(String key);
 }
