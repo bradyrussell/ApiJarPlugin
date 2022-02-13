d7637ef75cdb68901b80bb7f039e89fe84eb7823
BradyRussell <31458670+bradyrussell@users.noreply.github.com>
2022-02-12 19:36:44 -0600

    add methods to ApiJarPlugin

--- src/main/java/com/bradyrussell/apijar/runtime/ApiJarPlugin.java
+++ src/main/java/com/bradyrussell/apijar/runtime/ApiJarPlugin.java
@@ -2,5 +2,11 @@ package com.bradyrussell.apijar.runtime;
 
 public interface ApiJarPlugin {
     String getName();
-
+    String getDescription();
+    long getVersion();
+    default long getInitializeOrder() {
+        return 1;
+    }
+    boolean initialize();
+    void shutdown();
 }
