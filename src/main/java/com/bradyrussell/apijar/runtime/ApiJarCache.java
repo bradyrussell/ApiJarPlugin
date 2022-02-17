35f0d7c6e4304a91d6b086d87bca2a900107c446
BradyRussell <31458670+bradyrussell@users.noreply.github.com>
2022-02-17 10:10:15 -0600

    add cache duration

--- src/main/java/com/bradyrussell/apijar/runtime/ApiJarCache.java
+++ src/main/java/com/bradyrussell/apijar/runtime/ApiJarCache.java
@@ -1,13 +1,15 @@
 package com.bradyrussell.apijar.runtime;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.function.Supplier;
 
 public interface ApiJarCache {
     String get(String key);
-    void set(String key, String value);
+    void set(String key, String value, long duration);
     boolean has(String key);
+    Instant expires(String key);
     List<String> keys();
-    String getOrDefault(String key, String ifNotFound, boolean cacheDefaultIfNotFound);
-    String getOrCall(String key, Supplier<String> ifNotFound, boolean cacheDefaultIfNotFound);
+    String getOrDefault(String key, String ifNotFound, boolean cacheDefaultIfNotFound, long defaultDuration);
+    String getOrCall(String key, Supplier<String> ifNotFound, boolean cacheDefaultIfNotFound, long defaultDuration);
 }
